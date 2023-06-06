67541033c5dec186aa565f82403e2a1fb0d32dd6
paraIncog <selgeandreas02@gmail.com>
2023-06-06 12:59:53 +0300

    Somewhat fix donation

--- src/ShoppingCart/AddItem.js
+++ src/ShoppingCart/AddItem.js
@@ -1,25 +1,26 @@
 import { useState } from "react"
-import M from "materialize-css"
+// import M from "materialize-css"
 
 import carrotImage from '../itemPictures/carrot_image.jpg'
 import appleImage from '../itemPictures/apple_image.jpg'
 import steakImage from '../itemPictures/steak_image.jpg'
 import PopupModal from "./PopupModal"
 
-function initModal() {
-    var elems = document.querySelectorAll('.modal');
-    M.Modal.init(elems, {});
-}
+// function initModal() {
+//     let elems = document.querySelectorAll('.modal');
+//     M.Modal.init(elems, {});
+// }
 
-let donatesApples = false
+//  let donatesApples = false
 
 function AddItem({ addItem, item }) {
-    setTimeout(initModal, 200)
+    // setTimeout(initModal, 200)
 
     let [itemName, setItemName] = useState('')
     let [itemQuantity, setItemQuantity] = useState()
     let [itemPrice, setItemPrice] = useState()
     let [itemTotalPrice, setItemTotalPrice] = useState(itemQuantity * itemPrice)
+    // let [inputDisabled, setInputDisabled] = useState(0)
 
     const addProductData = (event) => {
         console.log('addProductData', itemPrice, itemQuantity)
@@ -52,36 +53,47 @@ function AddItem({ addItem, item }) {
         setItemTotalPrice(itemQuantity * itemPrice)
     }
 
-    
-    function donationModal() {
-        function donatesApplesYes() {
-            donatesApples = true
-            console.log('Willing to donate')
-            setItemName('Apple with Donation')
-            setItemQuantity(1 * 2)
-            setItemPrice(0.55 * 2)
-            setItemTotalPrice(itemQuantity * itemPrice)
-            addItem(itemName, itemQuantity, itemPrice, itemTotalPrice)
-        }
-    console.log('Donation Modal Activated')
-    if (donatesApples === false) {
-        console.log('Has not donated')
-    return (
-        <div id="modalDonation" class="modal">
-            <div class="modal-content">
-                <h6 className="center-align">Would You like to donate Carrots (1.21 €) to Foodbank?</h6>
-                <h6 className="row center-align">You'll get a discount for additional Carrots.</h6>
-                <div className="row">
-                    <button onClick={donatesApplesYes} className="btn col s6 green">Yes</button>
-                    <button href="#!" className="modal-close btn col s6 red">No</button>
-                </div>
-            </div>
-            {donationModal}
-        </div>
-    )
+    const addApplestoDonation = () => {
+        setItemName('Apple for Donation')
+        // setInputDisabled(1)
+        setItemQuantity(1)
+        setItemPrice(0.55 * 2)
+        setItemTotalPrice(itemQuantity * itemPrice)
     }
-}
 
+
+    // function donationModal() {
+    //     function donatesApplesYes() {
+    //         if (donatesApples === true) {
+    //             console.log('Willing to donate')
+    //             setTimeout(200)
+    //             setItemName('Apple with Donation')
+    //             setItemQuantity(1 * 2)
+    //             setItemPrice(0.55 * 2)
+    //             setItemTotalPrice(itemQuantity * itemPrice)
+    //             console.log(itemName, itemQuantity, itemPrice, itemTotalPrice)
+    //             console.log(addApplestoDonation)
+    //         }
+    //         addItem(itemName, itemQuantity, itemPrice, itemTotalPrice)
+    //     }
+
+    //     console.log('Donation Modal Activated')
+    //     if (donatesApples === false) {
+    //         console.log('Has not donated')
+    //         return (
+    //             <div id="modalDonation" class="modal">
+    //                 <div class="modal-content">
+    //                     <h6 className="center-align">Would You like to donate Apples (0.55 €) to Foodbank?</h6>
+    //                     <h6 className="row center-align">You'll get a discount for additional Apples.</h6>
+    //                     <div className="row">
+    //                         <button onClick={donatesApplesYes} className="modal-close btn col s6 green">Yes</button>
+    //                         <button href="#!" className="modal-close btn col s6 red">No</button>
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         )
+    //     }
+    // }
     return (
         <div className="goodPadding card-panel black-text">
             <form onSubmit={addProductData}>
@@ -139,14 +151,14 @@ function AddItem({ addItem, item }) {
                         </div>
                     </div>
                 </div>
-                {/* <div className="col s4">
-                    <div onClick={donationModal} data-target="modalDonation" className="modal-trigger">
+                <div className="col s4">
+                    <div onClick={addApplestoDonation}>
                         <img src={appleImage} alt='Apple' className="itemImage row" />
                         <div className="row">
-                            Add 1 donation Apple
+                            Donate apple to Foodbank
                         </div>
                     </div>
-                </div> */}
+                </div>
                 <div className="col s4">
                     <div onClick={addSteak} className="">
                         <img src={steakImage} alt='Steak' className="itemImage row" />
@@ -156,7 +168,6 @@ function AddItem({ addItem, item }) {
                     </div>
                 </div>
             </div>
-            {donationModal}
             {/* <div>
                 <div className="row center-align">
                     <button>
@@ -164,7 +175,7 @@ function AddItem({ addItem, item }) {
                     </button>
                 </div>
             </div> */}
-            {/* <PopupModal /> */}
+            <PopupModal />
         </div>
 
         // <div className="goodPadding yellow">
@@ -203,7 +214,7 @@ function AddItem({ addItem, item }) {
         //         </div>
         //     </div>
         // </div>
-        
+
     )
 }
 
--- src/ShoppingCart/PopupModal.js
+++ src/ShoppingCart/PopupModal.js
@@ -1,24 +1,41 @@
-import M from "materialize-css";
+// import M from "materialize-css";
 
-function initModal() {
-    console.log('Call initModal')
-    let elems = document.querySelectorAll('.modal');
-    let instances = M.Modal.init(elems, {});
+
+// function initModal() {
+//     console.log('Call initModal')
+//     let elems = document.querySelectorAll('.modal');
+//     // let instances = M.Modal.init(elems, {});
+// }
+
+let donatesApples = false
+console.log('Donation Modal Activated')
+if (donatesApples === false) {
+    console.log('Has not donated')
 }
 
+
+
+function donatesApplesYes() {
+    setTimeout(200)
+    donatesApples = true
+    console.log('Willing to donate')
+    // setItemName('Apple with Donation')
+    // setItemQuantity(1 * 2)
+    // setItemPrice(0.55 * 2)
+    // setItemTotalPrice(itemQuantity * itemPrice)
+}
+
+
 function PopupModal() {
-    setTimeout(initModal, 200)
+    //  setTimeout(initModal, 200)
     console.log()
     return (
-        <div id="modalDonation" class="modal">
-            <div class="modal-content">
-                <h6 className="center-align">Would You like to donate Carrots (1.21 €) to Foodbank?</h6>
-                <h6 className="row center-align">You will get a discount for additional Carrots.</h6>
-                <button>Yes</button>
-                <button href="#!" className="modal-close">No</button>
-            </div>
-            <div class="modal-footer">
-                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+        <div id="modalDonation" className="modal">
+            <div className="modal-content">
+                <h6 className="center-align">Would You like to donate Apple (1.21 €) to Foodbank?</h6>
+                <h6 className="row center-align">You will get a discount for additional Apples.</h6>
+                <button onClick={donatesApplesYes} className="green btn col s6 modal-close">Yes</button>
+                <button href="#!" className="red btn col s6 modal-close">No</button>
             </div>
         </div>
     )
