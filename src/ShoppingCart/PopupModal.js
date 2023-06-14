a1e20548f5148cda91772c84b76a0540f3c6bced
paraIncog <selgeandreas02@gmail.com>
2023-06-14 17:18:41 +0300

    Make example donation request

--- src/ShoppingCart/PopupModal.js
+++ src/ShoppingCart/PopupModal.js
@@ -10,8 +10,8 @@ function PopupModal({ yesDonation, noDonation }) {
     return (
         <div id="modalDonation" className="modal">
             <div className="modal-content">
-                <h6 className="center-align">Would You like to donate Apple (1.21 €) to Foodbank?</h6>
-                <h6 className="row center-align">You will get a discount for additional Apples.</h6>
+                <h6 className="center-align">Would You like to donate productName (productPrice) to Foodbank?</h6>
+                <h6 className="row center-align">You will get a discount for additional productName.</h6>
                 <button onClick={yesDonation} className="green btn col s6 modal-close donation-confirm">Yes</button>
                 <button onClick={noDonation} href="#!" className="red btn col s6 modal-close">Not Now</button>
             </div>
--- src/ShoppingCart/WelcomingPopupModal.js
+++ src/ShoppingCart/WelcomingPopupModal.js
@@ -11,9 +11,20 @@ function WelcomingPopupModal() {
         <div id="modalWelcomeDonation" className="modal">
             <div className="modal-content black-text">
                 <h6 className="center-align">These are the items Foodbank wants you to donate.</h6>
-            </div>
+                <div>
+                    <div
+                        className='goodPadding'
+                    // key={item.id}
+                    >
+                        <div className='row col s12'>
+                            <div className='col s6 goodVisText'>Apples</div>
+                            <div className='col s6 right-align goodVisText'>0.55 €</div>
+                        </div>
+                    </div>
+                </div>
                 <button href="#!" className="green btn col s6 modal-close">Yes</button>
                 <button href="#!" className="red btn col s6 modal-close">Not now</button>
+            </div>
         </div>
     )
 }
