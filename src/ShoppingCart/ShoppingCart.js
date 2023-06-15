714027f966b0aee097b88286d35a6961f220446e
paraIncog <selgeandreas02@gmail.com>
2023-06-15 23:10:12 +0300

    Remove Redundancies

--- src/ShoppingCart/ShoppingCart.js
+++ src/ShoppingCart/ShoppingCart.js
@@ -1,7 +1,6 @@
 import { useState } from "react"
 
 import CartItem from "./CartItem"
-// import EmptyCart from "./EmptyCart"
 import TallyFooter from "./TallyFooter"
 import UserHeader from "./UserHeader"
 import AddItem from "./AddItem"
@@ -10,76 +9,6 @@ function ShoppingCart() {
     let productQuantity;
     let productPrice;
     const [product, setProduct] = useState([
-        // {
-        //     id: 1,
-        //     productName: 'Carrot Kadarbik 500g',
-        //     productPrice: 1.21,
-        //     productQuantity: 1,
-        //     isDonatable: false
-        // },
-        // {
-        //     id: 2,
-        //     productName: 'Apple Royal Gala sweet, 1kilo 4pc',
-        //     productPrice: 0.55,
-        //     productQuantity: 1,
-        //     isDonatable: 1
-        // },
-        // {
-        //     id: 3,
-        //     productName: 'Broiler breast fillet insert Rimi GL 500g',
-        //     productPrice: 3.86,
-        //     productQuantity: 2,
-        //     isDonatable: 0
-        // },
-        // {
-        //     id: 4,
-        //     productName: 'Paprika mix Rimi 1kl, 350g',
-        //     productPrice: 3.86,
-        //     productQuantity: 1,
-        //     isDonatable: false
-        // },
-        // {
-        //     id: 5,
-        //     productName: 'Duck Pate 160g',
-        //     productPrice: 4.79,
-        //     productQuantity: 5,
-        //     isDonatable: 0
-        // },
-        // {
-        //     id: 6,
-        //     productName: 'Canned beef 240g',
-        //     productPrice: 4.3,
-        //     productQuantity: 1,
-        //     isDonatable: false
-        // },
-        // {
-        //     id: 7,
-        //     productName: 'Chicken in jelly "Villa vita"',
-        //     productPrice: 1.8,
-        //     productQuantity: 1,
-        //     isDonatable: false
-        // },
-        // {
-        //     id: 8,
-        //     productName: 'Milk chocolate PERGALE with nuts',
-        //     productPrice: 5.51,
-        //     productQuantity: 2,
-        //     isDonatable: false
-        // },
-        // {
-        //     id: 9,
-        //     productName: 'Dried Apricots',
-        //     productPrice: 6.49,
-        //     productQuantity: 1,
-        //     isDonatable: 0
-        // },
-        // {
-        //     id: 10,
-        //     productName: 'Gingerballs',
-        //     productPrice: 3.79,
-        //     productQuantity: 2,
-        //     isDonatable: false
-        // }
   ], {productTotalPrice: productPrice * productQuantity}
   )
 
@@ -92,7 +21,6 @@ function ShoppingCart() {
         <div className="col s12">
             <div className="container">
             <UserHeader />
-            {/* <EmptyCartEnable /> */}
             <CartItem product={product} />
             <AddItem handleAddItem={onItemAdd} />
             <div className='row deadSpace' />
--- src/ShoppingCart/UserHeader.js
+++ src/ShoppingCart/UserHeader.js
@@ -1,38 +1,11 @@
-// import globe from '../icons/globe-solid1.png'
-// import donation from '../icons/hand-holding-dollar-solid.png';
-
-// import WelcomingPopupModal from './WelcomingPopupModal'
-
 function UserHeader() {
-    
-    // const welcomeDonationModal = () => {
-    //     console.log('Welcome Donation Ballot Initiated')
-    // }
-
     return (
-        // <div className="col s12 row Header stickyHeader goodVisText goodPadding">
-        //     <h4 className="col s6 left-align">
-        //         Märt
-        //     </h4>
-        //     <div className="col s6 right-align modal-trigger" onClick={welcomeDonationModal} data-target="modalWelcomeDonation">
-        //         <img src={donation} alt="globe" className='globeItem' />
-        //     </div>
-        //     <WelcomingPopupModal />
-        // </div>
         <div className='row'>
             <div className='col s12 m12'>
                 <div className='card white black-text center-align'>
                     <div className='row'>
-                        {/* <h6>
-                            Märt
-                        </h6> */}
                     </div>
                     <div className='row'>
-                        {/* <div className='row'>
-                            <h4 className='red-text'>
-                                0,00
-                            </h4>
-                        </div> */}
                         <div className='row'>
                             <h5 className='bold'>
                                 Welcome
